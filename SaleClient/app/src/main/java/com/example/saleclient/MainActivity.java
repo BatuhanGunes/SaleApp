e709ca1b023193a875d255d4105a94ea2a04b33e
Batuhan Güneş <batuhangunes001@gmail.com>
2020-08-15 10:52:55 +0300

    Activity design created

--- SaleClient/app/src/main/java/com/example/saleclient/MainActivity.java
+++ SaleClient/app/src/main/java/com/example/saleclient/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.saleclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonConnect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonConnect = (Button)findViewById(R.id.buttonConnect);
+
+        buttonConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
